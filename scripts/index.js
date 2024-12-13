a5917be326de2ae9da067d5d17a3ad7384cac447
Daniel Lunev <lunev.danik@gmail.com>
2024-12-13 21:47:24 +0300

    review

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,12 @@ const cardTemplate = document.querySelector("#card-template").content;
 // @todo: DOM узлы
 const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
-function addCard(name, link) {
+function addCard(item, deleteCard) {
     const card = cardTemplate.querySelector(".card").cloneNode(true);
-    card.querySelector(".card__title").textContent = name;
-    card.querySelector(".card__image").setAttribute("src", link);
-    card.querySelector(".card__image").setAttribute("alt", `${name}`);
+    card.querySelector(".card__title").textContent = item.name;
+    const cardImage = card.querySelector(".card__image");
+    cardImage.setAttribute("src", item.link);
+    cardImage.setAttribute("alt", `${item.name}`);
     card.querySelector('.card__delete-button').addEventListener('click', deleteCard);    
     return card;
 }
@@ -16,4 +17,4 @@ function deleteCard(evt) {
     evt.target.closest('.card').remove();
 }
 // @todo: Вывести карточки на страницу
-initialCards.forEach(item => placesList.append(addCard(item.name, item.link)));
+initialCards.forEach(item => placesList.append(addCard(item, deleteCard)));
