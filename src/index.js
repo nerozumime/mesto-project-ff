967804e200cd88f4f7b08b15489406e7a2450c34
Daniel Lunev <lunev.danik@gmail.com>
2025-01-15 17:31:57 +0300

    webpack_v1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-const numbers = [2, 3, 5];
+import './pages/index.css'; // добавьте импорт главного файла стилей 
 
-// Стрелочная функция. Не запнётся ли на ней Internet Explorer?
-const doubledNumbers = numbers.map(number => number * 2);
-
-console.log(doubledNumbers); // 4, 6, 10
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+// @todo: Функция создания карточки
+function addCard(item, deleteCard) {
+    const card = cardTemplate.querySelector(".card").cloneNode(true);
+    card.querySelector(".card__title").textContent = item.name;
+    const cardImage = card.querySelector(".card__image");
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(card));    
+    return card;
+}
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {  
+    cardElement.remove(); 
+}; 
+// @todo: Вывести карточки на страницу
+initialCards.forEach(item => placesList.append(addCard(item, deleteCard)));
