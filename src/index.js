9d74a4f948f6a0872d951d68c24db70653f20746
Daniel Lunev <lunev.danik@gmail.com>
2025-01-20 20:52:21 +0300

    popup_begin

--- src/index.js
+++ src/index.js
@@ -1,2 +1,54 @@
 import './pages/index.css'; // импорт главного файла стилей 
-import {initialCards} from './scripts/cards'
+import {initialCards, addCard, deleteCard} from './scripts/cards'
+
+// @todo: DOM узлы
+const placesList = document.querySelector(".places__list");
+// @todo: Вывести карточки на страницу
+initialCards.forEach(item => placesList.append(addCard(item, deleteCard)));
+
+
+
+
+
+
+const buttonProfileEdit = document.querySelector('.profile__edit-button');
+const popupProfileEdit = document.querySelector('.popup_type_edit');
+const buttonPopupClose = document.querySelector('.popup__close');
+let currentlyOpenedModal;
+
+function closeModal(){
+  currentlyOpenedModal.classList.remove('popup_is-opened');
+  currentlyOpenedModal = null;
+  buttonPopupClose.removeEventListener('click', closeModal);
+}
+
+function closeModalWithEsc(evt){
+  if(evt.key == 'Escape'){
+    closeModal();
+    document.removeEventListener('keydown', closeModalWithEsc);
+  }
+}
+
+buttonProfileEdit.addEventListener('click', ()=> {
+  popupProfileEdit.classList.add('popup_is-opened');
+  currentlyOpenedModal = popupProfileEdit;
+  buttonPopupClose.addEventListener('click', closeModal);
+  document.addEventListener('keydown', closeModalWithEsc);
+});
+
+
+
+
+/*
+
+document.addEventListener('keydown', (evt)=> { 
+  console.log(evt.key);
+}); 
+
+function closeOnBackDropClick({ currentTarget, target }) {
+  const isClickedOnBackDrop = target === currentTarget;
+  if (isClickedOnBackDrop){
+    closeModal();
+  }
+}
+*/
--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,5 +1,3 @@
-// const archyz = new URL('https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg', import.meta.url); 
-
 export const initialCards = [
     {
       name: "Архыз",
@@ -26,3 +24,21 @@ export const initialCards = [
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
     }
 ];
+
+// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+// @todo: Функция создания карточки
+export function addCard(item, deleteCard) {
+    const card = cardTemplate.querySelector(".card").cloneNode(true);
+    card.querySelector(".card__title").textContent = item.name;
+    const cardImage = card.querySelector(".card__image");
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    card.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(card));    
+    return card;
+}
+// @todo: Функция удаления карточки
+export function deleteCard(cardElement) {  
+    cardElement.remove(); 
+}; 
+
