81bd111a83a1d92d9a5361c2e351de981067ea72
Daniel Lunev <lunev.danik@gmail.com>
2025-01-21 11:47:08 +0300

    backdrop click close

--- src/index.js
+++ src/index.js
@@ -14,41 +14,29 @@ initialCards.forEach(item => placesList.append(addCard(item, deleteCard)));
 const buttonProfileEdit = document.querySelector('.profile__edit-button');
 const popupProfileEdit = document.querySelector('.popup_type_edit');
 const buttonPopupClose = document.querySelector('.popup__close');
-let currentlyOpenedModal;
+const popupContent = document.querySelector('.popup__content');
+
+popupContent.addEventListener('click', (evt)=> evt.stopPropagation());
 
 function closeModal(){
-  currentlyOpenedModal.classList.remove('popup_is-opened');
-  currentlyOpenedModal = null;
-  buttonPopupClose.removeEventListener('click', closeModal);
+    document.querySelector('.popup_is-opened').removeEventListener('click', closeModal);
+    document.querySelector('.popup_is-opened').classList.remove('popup_is-opened');
+    buttonPopupClose.removeEventListener('click', closeModal);
+    document.removeEventListener('keydown', closeModalWithEsc);
 }
+
 
 function closeModalWithEsc(evt){
   if(evt.key == 'Escape'){
     closeModal();
-    document.removeEventListener('keydown', closeModalWithEsc);
   }
 }
 
-buttonProfileEdit.addEventListener('click', ()=> {
-  popupProfileEdit.classList.add('popup_is-opened');
-  currentlyOpenedModal = popupProfileEdit;
+function openModal(modal){
+  modal.classList.add('popup_is-opened');
   buttonPopupClose.addEventListener('click', closeModal);
   document.addEventListener('keydown', closeModalWithEsc);
-});
-
-
-
-
-/*
-
-document.addEventListener('keydown', (evt)=> { 
-  console.log(evt.key);
-}); 
-
-function closeOnBackDropClick({ currentTarget, target }) {
-  const isClickedOnBackDrop = target === currentTarget;
-  if (isClickedOnBackDrop){
-    closeModal();
-  }
+  document.querySelector('.popup_is-opened').addEventListener('click', closeModal);
 }
-*/
+
+buttonProfileEdit.addEventListener('click', ()=> openModal(popupProfileEdit));
